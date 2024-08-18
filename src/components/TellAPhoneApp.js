c4e53e8cba3840b127472c252567adc49447e1d2
Noah <noah.little@usask.ca>
2024-08-18 10:54:51 -0400

    refactor: Fix indentation issue in TellAPhoneApp.js

--- src/components/TellAPhoneApp.js
+++ src/components/TellAPhoneApp.js
@@ -357,7 +357,7 @@ const TellAPhoneApp = () => {
             </DialogTitle>
           </DialogHeader>
           <div className="grid gap-4 py-4">
-            <div className="flex flex-col space-y-1.5">
+          <div className="flex flex-col space-y-1.5">
               <Label htmlFor="username">Choose Your Username</Label>
               <Input
                 id="username"
@@ -501,7 +501,6 @@ const TellAPhoneApp = () => {
                 </div>
               )}
             </div>
-
             <div className="flex items-center justify-between text-gray-300">
               <div className="flex items-center">
                 <Users className="w-5 h-5 mr-2 text-blue-400" />
